af1e8408dadce72a4d9f4d09373e838a5d7bb62b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-07-09 08:14:15 +0200

    Implement new methods in react/cache 0.6

--- tests/MsgpackTest.php
+++ tests/MsgpackTest.php
@@ -63,4 +63,74 @@ final class MsgpackTest extends AsyncTestCase
         $jsonCache = new Msgpack($cache->reveal());
         $jsonCache->delete($key);
     }
+
+    public function testGetMultiple(): void
+    {
+        $key = 'sleutel';
+        $json = [
+            'foo' => 'bar',
+        ];
+        $string = msgpack_pack($json);
+
+        $cache = $this->prophesize(CacheInterface::class);
+        $cache->getMultiple([$key], null)->shouldBeCalled()->willReturn(resolve([$key => $string]));
+
+        $jsonCache = new Msgpack($cache->reveal());
+        self::assertSame([$key => $json], $this->await($jsonCache->getMultiple([$key])));
+    }
+
+    public function testGetMultipleNullShouldBeIgnored(): void
+    {
+        $key = 'sleutel';
+
+        $cache = $this->prophesize(CacheInterface::class);
+        $cache->getMultiple([$key], null)->shouldBeCalled()->willReturn(resolve([$key => null]));
+
+        $jsonCache = new Msgpack($cache->reveal());
+        self::assertNull(\current($this->await($jsonCache->getMultiple([$key]))));
+    }
+
+    public function testSetMultiple(): void
+    {
+        $key = 'sleutel';
+        $json = [
+            'foo' => 'bar',
+        ];
+        $string = msgpack_pack($json);
+
+        $cache = $this->prophesize(CacheInterface::class);
+        $cache->setMultiple([$key => $string], null)->shouldBeCalled();
+
+        $jsonCache = new Msgpack($cache->reveal());
+        $jsonCache->setMultiple([$key => $json]);
+    }
+
+    public function testDeleteMultiple(): void
+    {
+        $key = 'sleutel';
+        $cache = $this->prophesize(CacheInterface::class);
+        $cache->deleteMultiple([$key], null)->shouldBeCalled();
+
+        $jsonCache = new Msgpack($cache->reveal());
+        $jsonCache->deleteMultiple([$key]);
+    }
+
+    public function testClear(): void
+    {
+        $cache = $this->prophesize(CacheInterface::class);
+        $cache->clear()->shouldBeCalled();
+
+        $jsonCache = new Msgpack($cache->reveal());
+        $jsonCache->clear();
+    }
+
+    public function testHas(): void
+    {
+        $key = 'sleutel';
+        $cache = $this->prophesize(CacheInterface::class);
+        $cache->has($key, null)->shouldBeCalled();
+
+        $jsonCache = new Msgpack($cache->reveal());
+        $jsonCache->has($key);
+    }
 }
